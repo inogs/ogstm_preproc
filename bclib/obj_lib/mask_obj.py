d7cfa81152766ecc93d52216758681e63e2e21fe
epascolo <eric.pascolo@gmail.com>
2015-10-21 15:31:22 +0200

    BF lateral_bc.convert_information debugged

--- bclib/obj_lib/mask_obj.py
+++ bclib/obj_lib/mask_obj.py
@@ -29,10 +29,9 @@ class lateral_bc:
         
     def _convert_information(self):
         jpt = 4
-        size_nutrients = np.zeros(5)
+        size_nutrients = np.zeros(4,dtype = np.int)
         size_nutrients[0] = jpt
-        size_nutrients[1:5] = self._mesh_father.tmask_dimension
-        print(size_nutrients)
+        size_nutrients[1:] = self._mesh_father.tmask_dimension[1:]
         self.phos = np.zeros(size_nutrients)
         self.ntra = np.zeros(size_nutrients)
         self.dox = np.zeros(size_nutrients)
@@ -41,54 +40,58 @@ class lateral_bc:
         self.alk = np.zeros(size_nutrients)
         tmask4d = np.zeros(size_nutrients)
         
-        n_lev = size_nutrients[2]
-        # vp_phos = zeros(4,nLev); 
+        n_lev = self._mesh_father.tmask_dimension[1]
+        vp_phos = np.zeros((4,n_lev)); 
         vp_ntra = np.zeros((4,n_lev)); 
         vp_sica = np.zeros((4,n_lev)); 
         vp_dox  = np.zeros((4,n_lev)); 
         vp_dic  = np.zeros((4,n_lev));
         vp_alk  = np.zeros((4,n_lev));
-        nav_lev_in=np.matrix((0,self.nav_lev_in,5500)); 
-        vp_phos_in=np.matrix((self.N1p[:,1],self.N1p,self.N1p[:,self.lev1-1]));
-        vp_ntra_in=np.matrix((self.N3n[:,1],self.N3n,self.N3n[:,self.lev1-1]));
-        vp_sica_in=np.matrix((self.N5s[:,1],self.N5s,self.N5s[:,self.lev1-1]));
-        vp_dox_in =np.matrix((self.O2o[ :,1],self.O2o,self.O2o[:,self.lev1-1]));
-        end = self.ALKDIC.shape[0]
-        nav_lev_in2 =[ 0,self.ALKDIC[:,1].T,5500];
-        vp_dic_in   =np.matrix(self.ALKDIC[1,3],self.ALKDIC[:,3].T,self.ALKDIC[end-1,3]); 
-        vp_alk_in   =np.matrix(self.ALKDIC[1,2],self.ALKDIC[:,2].T,self.ALKDIC[end-1,2]);
-#  
-        vp_phos = np.zeros(4,self._mesh_father.nav_lev)
-        vp_ntra = np.zeros(4,self._mesh_father.nav_lev)
-        vp_dox = np.zeros(4,self._mesh_father.nav_lev)
-        vp_sica = np.zeros(4,self._mesh_father.nav_lev)
-        vp_dic = np.zeros(4,self._mesh_father.nav_lev)
-        vp_alk = np.zeros(4,self._mesh_father.nav_lev)
         
+        nav_lev_in=np.concatenate(([0],self.lev1,[5500]))
+        
+        vp_phos_in=np.column_stack((self.N1p[:,1],self.N1p,self.N1p[:,len(self.lev1)-2]))
+        
+        vp_ntra_in=np.column_stack((self.N3n[:,1],self.N3n,self.N3n[:,len(self.lev1)-2]))
+                             
+        vp_sica_in=np.column_stack((self.N5s[:,1],self.N5s,self.N5s[:,len(self.lev1)-2]))
+        
+        vp_dox_in=np.column_stack((self.O2o[ :,1],self.O2o,self.O2o[:,len(self.lev1)-2]))
+        end = len(self.ALK)
+        
+        nav_lev_in2 = np.append([0],self.lev2.T)
+        nav_lev_in2 = np.append(nav_lev_in2,[5500])
+        
+        vp_dic_in = np.append(self.DIC[1],self.DIC[:].T)
+        vp_dic_in = np.append(vp_dic_in,self.DIC[end-1]) 
+        
+        vp_alk_in = np.append(self.ALK[1],self.ALK[:].T)
+        vp_alk_in = np.append(vp_alk_in,self.ALK[end-1]);
+                       
         for i in range(4):
-            jj= not np.isnan(vp_phos_in[i,:]);
-            vp_phos[i,:] = npint.interp1d(nav_lev_in[jj],vp_phos_in[i,jj]);
-            jj=not np.isnan(vp_ntra_in[i,:]);  
-            vp_ntra[i,:] = npint.interp1d(nav_lev_in[jj],vp_ntra_in[i,jj]);
-            jj=not np.isnan(vp_dox_in[i,:]);
-            vp_dox[i,:] = npint.interp1d(nav_lev_in[jj],vp_dox_in[ i,jj]);
-            jj=not np.isnan(vp_sica_in[i,:]);
-            vp_sica[i,:] = npint.interp1d(nav_lev_in[jj],vp_sica_in[i,jj]);
-            jj=not np.isnan(vp_dic_in[:]);
-            vp_dic[i,:]  = npint.interp1d(nav_lev_in2(jj),vp_dic_in(jj));
-            jj=not np.isnan(vp_alk_in[:]); 
-            vp_alk[i,:]  = npint.interp1d(nav_lev_in2[jj],vp_alk_in[jj]);
+            jj= (~ np.isnan(vp_phos_in[i,:])) | ( vp_phos_in[i,:] < 1e19 ) ;
+            vp_phos[i,:] = np.interp(self._mesh_father.nav_lev, nav_lev_in[jj], vp_phos_in[i,jj])
+            jj=~ np.isnan(vp_ntra_in[i,:]) | ( vp_ntra_in[i,:] < 1e19 );  
+            vp_ntra[i,:] = np.interp(self._mesh_father.nav_lev,nav_lev_in[jj],vp_ntra_in[i,jj]);
+            jj=~ np.isnan(vp_dox_in[i,:]) | ( vp_dox_in[i,:] < 1e19 );
+            vp_dox[i,:] = np.interp(self._mesh_father.nav_lev,nav_lev_in[jj],vp_dox_in[ i,jj]);
+            jj=~ np.isnan(vp_sica_in[i,:]) | ( vp_sica_in[i,:] < 1e19);
+            vp_sica[i,:] = np.interp(self._mesh_father.nav_lev,nav_lev_in[jj],vp_sica_in[i,jj]);
+            jj=~ np.isnan(vp_dic_in[:]) | ( vp_dic_in[:] < 1e19);
+            vp_dic[i,:]  = np.interp(self._mesh_father.nav_lev,nav_lev_in2[jj],vp_dic_in[jj]);
+            jj=~ np.isnan(vp_alk_in[:]) | ( vp_alk_in[:] < 1e19) ; 
+            vp_alk[i,:]  = np.interp(self._mesh_father.nav_lev,nav_lev_in2[jj],vp_alk_in[jj]);
 
 # 
 # %Loop on time seasonal
             for jt in range(jpt):
                 for jk in range(n_lev):
-                    self.phos[jt,jk,:,:] = vp_phos(jt,jk);
-                    self.ntra[jt,jk,:,:] = vp_ntra(jt,jk);
-                    self.dox[jt,jk,:,:] = vp_dox( jt,jk);
-                    self.sica[jt,jk,:,:] = vp_sica(jt,jk);
-                    self.dic[jt,jk,:,:]  = vp_dic(jt,jk);
-                    self.alk[jt,jk,:,:]  = vp_alk(jt,jk);
+                    self.phos[jt,jk][:,:] = vp_phos[jt,jk];
+                    self.ntra[jt,jk,:,:] = vp_ntra[jt,jk];
+                    self.dox[jt,jk,:,:] = vp_dox[jt,jk];
+                    self.sica[jt,jk,:,:] = vp_sica[jt,jk];
+                    self.dic[jt,jk,:,:]  = vp_dic[jt,jk];
+                    self.alk[jt,jk,:,:]  = vp_alk[jt,jk];
 
  
             for jt in range(jpt):
@@ -166,8 +169,7 @@ class river_data:
         [rows, cols] = np.nonzero(mask)
         # [rows, cols] = find(mask);
         nSea = len(rows);
-        coast = np.zeros(np.size(mask),dtype = np.bool)
-        coast=mask
+        coast=mask.astype(np.bool)
         
         for i in range(0,nSea):
             row=rows[i]
