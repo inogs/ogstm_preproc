f7bf82d043ad10bde96b4f1e8b57efc83601b28f
Paolo Lazzari <plazzari@ogs.trieste.it>
2016-08-24 12:21:18 +0200

    change json file name

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bclib.obj_lib import co2_obj as co2obj
 from bclib.io_lib import excel_obj as xobj
 
 
-elab = rconf.elaboration(json_input="./conf_example.json")
+elab = rconf.elaboration(json_input="./conf24.json")
 
 mesh = maskobj.mesh(elab)
 #mesh.generate_boundmask()
@@ -13,5 +13,5 @@ mesh = maskobj.mesh(elab)
 #mesh.submesh.atmosphere()
 #mesh.river.map_contribute_on_sea()
 #mesh.bc()
-# co2  = co2obj.co2atm(elab)
-# co2.generator(mesh)
+#co2  = co2obj.co2atm(elab)
+#co2.generator(mesh)
