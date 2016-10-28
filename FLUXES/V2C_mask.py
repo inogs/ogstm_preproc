7a5d4826ba2d0ee90eb54ca6d5623f2e8883c49d
Anna Teruzzi <ateruzzi@node001.pico.cineca.it>
2016-10-28 15:22:18 +0200

    Just adding

---
+++ FLUXES/V2C_mask.py
@@ -0,0 +1,13 @@
+# File that define paths for SO simulations mesh
+import numpy as np
+maskfile       = '/pico/scratch/userexternal/ateruzzi/RA_COAST/wrkdir/MODEL/meshmask.nc'
+bounmask       = '/pico/scratch/userexternal/ateruzzi/RA_COAST/wrkdir/MODEL/bounmask.nc'
+submaskfiledat = 'None'
+submaskfilenc  = '/pico/scratch/userexternal/plazzari/eas2_v3/wrkdir/MODEL/sbmask.nc'
+
+jpi = 722   #longitudinal size of the model grid
+jpj = 253   #latitudinal  size of the model grid
+jpk = 72   
+
+lon8 = np.NaN 
+lat8 = np.NaN 
