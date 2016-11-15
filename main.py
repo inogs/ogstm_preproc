9eb639efb90c00cb8048ca01e73c93efeca11319
Giorgio Bolzon <gbolzon@inogs.it>
2016-11-15 09:26:40 +0100

    Working state of main caller

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3.5
-from bclib.io_lib  import read_configure
-from bclib.obj_lib.bmask_obj import bmesh
-#from bclib.obj_lib import mask_obj as maskobj
 from commons.mask import Mask
+from bclib.io_lib  import read_configure
+from bclib.obj_lib.bounmask import bounmask
 from bclib.obj_lib import atmosphere
+from bclib.obj_lib.gib import gib
+from bclib.obj_lib.co2_obj import co2atm
 
 conf = read_configure.elaboration(json_input="./conf24.json")
-conf.file_mask="meshmask.nc"
-conf.dir_out="."
 TheMask = Mask(conf.file_mask)
 
+CO2 =co2atm(conf)
+CO2.generate(TheMask)
 ATM=atmosphere.atmosphere(TheMask,conf)
-
 ATM.write_netcdf(TheMask, conf.dir_out)
+
+
+BOUN = bounmask(conf)
+BOUN.generate(TheMask)
+BOUN.write_netcdf(TheMask)
+
+G = gib(conf,TheMask)
+G.generate(TheMask, BOUN)
+
+
+
+
+
 import sys
 sys.exit()
+## RIVER, to be tested
+index = BOUN.load('index')
 
-
-B=bmesh(conf.file_bmask, conf)
-index = B.load_bounmask()
-
-conf.file_river= "input_obc_eas2_v3.xlsx"
 from bclib.obj_lib.river_obj import river_data
 R = river_data(conf)
 R.modularize(conf)
 georef = R.gen_map_indexes(TheMask)
 idxt, positions = R.gen_boun_indexes(index)
-import sys
-sys.exit()
-
-
 
 R.generate_monthly_files(conf, TheMask,idxt, positions)
 
@@ -38,15 +44,3 @@ if climatological:
     for sheet in conf.river_data_sheet:
         R.river_data[sheet]['yyyy'] = (R.river_data[sheet]['2000'] + R.river_data[sheet]['2001'] + R.river_data[sheet]['2002'])/3
     R.generate_climatological_monthly_files(conf, TheMask)
-
-
-
-
-#mesh = maskobj.mesh(elab)
-#mesh.generate_boundmask()
-#mesh.bounmesh.write_netcdf()
-#mesh.submesh.atmosphere()
-#mesh.river.map_contribute_on_sea()
-#mesh.bc()
-#co2  = co2obj.co2atm(elab)
-#co2.generator(mesh)
