d71f57ce6a540ee95b99e56747cf572e3e388ce0
Giorgio Bolzon <gbolzon@inogs.it>
2016-11-18 15:51:57 +0100

    Parallel version dom_dec 4x4

--- Climatologies/KD_490/climatology.py
+++ Climatologies/KD_490/climatology.py
@@ -4,6 +4,16 @@ from commons.Timelist import TimeList
 from commons.time_interval import TimeInterval
 import dom_dec
 import time_manager
+try:
+    from mpi4py import MPI
+    comm  = MPI.COMM_WORLD
+    rank  = comm.Get_rank()
+    nranks =comm.size
+    isParallel = True
+except:
+    rank   = 0
+    nranks = 1
+    isParallel = False
 
 maskfile="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/KD490_1km_meshmask.nc"
 INPUTDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/DAILY/ORIG/"
@@ -15,52 +25,58 @@ D.close()
 
 jpjglo,jpiglo = tmask_glo.shape
 
-nproc_i = 2
-nproc_j = 2
+nproc_i = 4
+nproc_j = 4
+PROCESSES = np.arange(nproc_i*nproc_j)
 
-for jproc in range(nproc_j):
-    for iproc in range(nproc_i):
-        #if ((jj==0) & (ji==0)) : continue
-        inputfile = "Kd_raw_data_%d_%d.npy" %(jproc,iproc)
-        outputfile= "Kd_clim_%d_%d" %(jproc,iproc)
-        print inputfile
-        RAW_DATA = np.load(inputfile)
-        
-        
-        I_start,I_end, J_start, J_end = dom_dec.dom_dec(iproc, jproc, jpiglo, jpjglo, nproc_i, nproc_j)
-        tmask = tmask_glo[J_start:J_end, I_start:I_end]
-        print I_start,I_end, J_start, J_end
 
-        Nwaterpoints = tmask.sum()
-        WaterPoints = np.zeros_like(tmask, dtype=np.int32)
-        WaterPoints[tmask] = np.arange(Nwaterpoints)
-        print "Nwaterpoints", Nwaterpoints
-        jpi = I_end-I_start
-        jpj = J_end-J_start
-        CLIM = np.zeros((jpj,jpi,366), dtype=[('NUMB',np.int32), ('MEAN',np.float32),('STD',np.float32)])
-        for julian in range(366):
-            print julian
-            II, filelist=time_manager.getfilelist(julian)
-            raw_data_julian=RAW_DATA[:,II]
-            for ji in range(jpi):
-                for jj in range(jpj):
-                    if tmask[jj,ji]:
-                        jw = WaterPoints[jj,ji];
-                        PILAloc = raw_data_julian[jw,:];
-                        valid_points =PILAloc>-999
-                        if valid_points.sum() < 5: continue                        
-                        pilarray= PILAloc[valid_points];
+for ip in PROCESSES[rank::nranks]:
+    (jproc, iproc) = divmod(ip,nproc_i)
+    inputfile = "Kd_raw_data_%d_%d.npy" %(jproc,iproc)
+    outputfile= "Kd_clim_%d_%d" %(jproc,iproc)
+    I_start,I_end, J_start, J_end = dom_dec.dom_dec(iproc, jproc, jpiglo, jpjglo, nproc_i, nproc_j)
+    tmask = tmask_glo[J_start:J_end, I_start:I_end]
+    print "rank ", ip, "processes " , outputfile, iproc, jproc, I_start,I_end, J_start, J_end
+    Nwaterpoints = tmask.sum()
 
+    print inputfile
+    print "Nwaterpoints", Nwaterpoints, ip
+    if (Nwaterpoints ==0) : continue
+
+
+    RAW_DATA = np.load(inputfile)
+
+
+
+    WaterPoints = np.zeros_like(tmask, dtype=np.int32)
+    WaterPoints[tmask] = np.arange(Nwaterpoints)
+    print "Nwaterpoints", Nwaterpoints
+    jpi = I_end-I_start
+    jpj = J_end-J_start
+    CLIM = np.zeros((jpj,jpi,366), dtype=[('NUMB',np.int32), ('MEAN',np.float32),('STD',np.float32)])
+    for julian in range(366):
+        print julian
+        II, filelist=time_manager.getfilelist(julian)
+        raw_data_julian=RAW_DATA[:,II]
+        for ji in range(jpi):
+            for jj in range(jpj):
+                if tmask[jj,ji]:
+                    jw = WaterPoints[jj,ji];
+                    PILAloc = raw_data_julian[jw,:];
+                    valid_points =PILAloc>-999
+                    if valid_points.sum() < 5: continue
+                    pilarray= PILAloc[valid_points];
+
+                    m = pilarray.mean()
+                    s = pilarray.std()
+                    outsiders = pilarray > m+3.0*s
+                    pilarray=pilarray[~outsiders]
+                    n = len(pilarray)
+                    CLIM['NUMB'][jj,ji,julian] = n
+                    if n>4:
                         m = pilarray.mean()
-                        s = pilarray.std()
-                        outsiders = pilarray > m+3.0*s
-                        pilarray=pilarray[~outsiders]
-                        n = len(pilarray)
-                        CLIM['NUMB'][jj,ji,julian] = n
-                        if n>4:
-                            m = pilarray.mean()
-                            CLIM['MEAN'][jj,ji,julian] = m
-                            CLIM['STD' ][jj,ji,julian] =np.sqrt( ((pilarray-m)**2).sum()/(n-1)  )
-                        
-        np.save(outputfile,CLIM)
+                        CLIM['MEAN'][jj,ji,julian] = m
+                        CLIM['STD' ][jj,ji,julian] =np.sqrt( ((pilarray-m)**2).sum()/(n-1)  )
+
+    np.save(outputfile,CLIM)
         
--- Climatologies/KD_490/setup_rawdata.py
+++ Climatologies/KD_490/setup_rawdata.py
@@ -5,6 +5,17 @@ from commons.time_interval import TimeInterval
 import Sat.SatManager as Sat
 import dom_dec
 
+try:
+    from mpi4py import MPI
+    comm  = MPI.COMM_WORLD
+    rank  = comm.Get_rank()
+    nranks =comm.size
+    isParallel = True
+except:
+    rank   = 0
+    nranks = 1
+    isParallel = False
+
 maskfile="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/KD490_1km_meshmask.nc"
 INPUTDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/DAILY/ORIG/"
 TI = TimeInterval("19500101","20500101","%Y%m%d")
@@ -17,29 +28,31 @@ D.close()
 
 jpjglo,jpiglo = tmask_glo.shape
 
-nproc_i = 2
-nproc_j = 2
+nproc_i = 4
+nproc_j = 4
+PROCESSES = np.arange(nproc_i*nproc_j)
 
-for jj in range(nproc_j):
-    for ji in range(nproc_i):
-        outfile = "Kd_raw_data_%d_%d" %(jj,ji)
-        print outfile
-        #if ((jj==0) & (ji==0)) : continue
-        
-        I_start,I_end, J_start, J_end = dom_dec.dom_dec(ji, jj, jpiglo, jpjglo, nproc_i, nproc_j)
-        tmask = tmask_glo[J_start:J_end, I_start:I_end]
-        print I_start,I_end, J_start, J_end
 
-        Nwaterpoints = tmask.sum()
-        print "Nwaterpoints", Nwaterpoints
-        
-        RAW_DATA = np.zeros((Nwaterpoints,nFrames),np.float32)
-        for ifile, filename in enumerate(TL.filelist):
-            A=Sat.readfromfile(filename,'KD490')
-            A=A[J_start:J_end, I_start:I_end]
-            RAW_DATA[:,ifile] = A[tmask]
-        
-        np.save(outfile,RAW_DATA)
+for ip in PROCESSES[rank::nranks]:
+    (jproc, iproc) = divmod(ip,nproc_i)
+    outfile = "Kd_raw_data_%d_%d" %(jproc,iproc)
+
+    I_start,I_end, J_start, J_end = dom_dec.dom_dec(iproc, jproc, jpiglo, jpjglo, nproc_i, nproc_j)
+    tmask = tmask_glo[J_start:J_end, I_start:I_end]
+    print "rank ", ip, "processes " , outfile, iproc, jproc, I_start,I_end, J_start, J_end
+
+    Nwaterpoints = tmask.sum()
+    print "Nwaterpoints", Nwaterpoints, ip
+    if (Nwaterpoints ==0) : continue
+
+    RAW_DATA = np.zeros((Nwaterpoints,nFrames),np.float32)
+    for ifile, filename in enumerate(TL.filelist):
+        print "reading", ip, ifile
+        A=Sat.readfromfile(filename,'KD490')
+        A=A[J_start:J_end, I_start:I_end]
+        RAW_DATA[:,ifile] = A[tmask]
+
+    np.save(outfile,RAW_DATA)
 
 
 
