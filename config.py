c982d14b6ffab3234eead4664def120048ce8b54
Giorgio Bolzon <gbolzon@inogs.it>
2018-04-06 12:06:45 +0200

    IMPORTANT CHANGE on GIBRALTAR nudging zone.
    Longitude limits of the nudging decay zone have been reduced: from (7.5W - 5.25W) to (6.5W - 6.0W) for nutrients and from (5.5W - 5.25W) to (6.1W - 6.0W) for carbonate.
    Light and BFM work from 6.0W eastward ( atlantic_bfm=FALSE)
    Profiles of nitrate and phosphate reduced by 0.5 and 0.1 mmol/m3, respectively.
    Results of the changes: phosphate and nitrate profiles of water masses entering through Gibraltar are closer to those observed in the ALB subbasin (i.e. lower values at surface). As a consequence, chlorophyll and primary production in alb, swm1,swm2 and partially in nwm are lower.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # paths
 dir_out        = "out/"
-file_mask      = "/pico/scratch/userexternal/gbolzon0/eas_v12/eas_v19_3/wrkdir/MODEL/meshmask.nc"
+file_mask      = "/marconi_scratch/userexternal/gbolzon0/EAS3_06/wrkdir/MODEL/meshmask.nc"
 file_co2       = "CMIP5_scenarios_RCP_CO2_mixing_ratio.nc"
 file_nutrients = "VPnutrients_CO2.nc"
 file_bmask     = "out/bounmask.nc"
@@ -16,19 +16,19 @@ po4_eas = (  379. +   957)/2
 n3n_wes = (10042. + 72825)/2
 n3n_eas = ( 6064. + 73621)/2
 
-end_nudging=-5.25
+end_nudging=-6.0
 
 # bounmask resto settings
 rdpmin = 1./24
 rdpmax = 90.
 
-variables=[[u'N1p', -7.5],
- [u'N3n', -7.5],
+variables=[[u'N1p', -6.5],
+ [u'N3n', -6.5],
  [u'O2o', -7.5],
  [u'N5s', -7.5],
- [u'O3c', -5.5],
- [u'O3h', -5.5],
- [u'N6r', -5.5]]
+ [u'O3c', -6.1],
+ [u'O3h', -6.1],
+ [u'N6r', -6.1]]
 
 
 river_data_sheet = ['KM3perYR_NOBLS',
