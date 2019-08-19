bc986e64d7f465bf919463e6c00ec871e36f1e0f
Giorgio Bolzon <gbolzon@inogs.it>
2019-08-19 16:07:40 +0200

    Avoiding logs too much longs

--- bclib/__init__.py
+++ bclib/__init__.py
@@ -4,5 +4,5 @@
 
 """
 import logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
 logging.info("BOUNDARY CONDITIONS python version 1.0")
