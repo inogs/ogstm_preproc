b4b8259201eecbdd67638710d53c3bbea9f39c8c
Valeria Di Biagio <vdibiagi@r033c01s03.galileo.cineca.it>
2020-05-13 19:44:52 +0200

    Minor bug fix, parser added

--- atlantic/atlantic_generator.py
+++ atlantic/atlantic_generator.py
@@ -1,16 +1,48 @@
+import argparse
+
+def argument():
+    parser = argparse.ArgumentParser(description = '''Creates 3D files of BC in Atlantic,
+                               starting from netCDF profiles
+                              
+                               ''')
+    parser.add_argument(   '--inputdir', '-i',
+                                type = str,
+                                required = True,
+                                help ='/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/BC/inputs'
+                                )
+    parser.add_argument(   '--outputdir', '-o',
+                                type = str,
+                                required = True,
+                                help ='/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/BC/OUTPUT'
+                                )
+    parser.add_argument(   '--maskfile', '-m',
+                                type = str,
+                                required = True,
+                                help ='/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/MASK/ogstm/meshmask.nc'
+                                )
+
+    return parser.parse_args()
+
+
+args = argument()
+
 from commons.mask import Mask
 import numpy as np
 from commons import netcdf4
+from commons.utils import addsep
+import os
 
-INPUTDIR="/gpfs/scratch/userexternal/vdibiagi/ogstm_boundary_conditions/atlantic/gen_inputs_BC/PROFILES/"
-OUTPUTDIR="/gpfs/scratch/userexternal/vdibiagi/ogstm_boundary_conditions/atlantic/BC_atl_boundary/"
+INPUTDIR  = addsep(args.inputdir)
+OUTPUTDIR  = addsep(args.outputdir)
+MASKFILE=args.maskfile
 
-#check 
-OpenMask=Mask('/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/MASK/ogstm/meshmask.nc')
+os.system("mkdir -p " + OUTPUTDIR)
+
+
+OpenMask=Mask(MASKFILE)
 
 jpk, jpj, jpi = OpenMask.shape
-#check 
-I=1
+I=0
 
 VARLIST=['N1p', 'N3n', 'N5s','O2o','O3c','O3h']
 mydtype=[(var,np.float32) for var in VARLIST]
@@ -26,7 +58,7 @@ for var in VARLIST:
     for k in range(jpk):
         BOUNDARY[k,:,I] = BOUNDARY_CONCENTRATION[var][k]
     BOUNDARY[~OpenMask.mask]=1.e+20
-    outfile=OUTPUTDIR + "OPE_yyyy0630-00:00:00.nc"
+    outfile=OUTPUTDIR + "ATL_yyyy0630-00:00:00.nc"
     netcdf4.write_3d_file(BOUNDARY, var, outfile, OpenMask,compression=True)
 
 
