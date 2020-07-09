077534c5d6bfc7732f2ee5aad838ef96dd8d0fea
Giorgio Bolzon <gbolzon@inogs.it>
2020-07-09 15:29:17 +0200

    1. Generalization for CHL and KD490 2. Use of argparse

--- Climatologies/KD_490/mask_and_counter.py
+++ Climatologies/KD_490/mask_and_counter.py
@@ -1,18 +1,50 @@
+import argparse
+def argument():
+    parser = argparse.ArgumentParser(description = '''
+    Step 1 for the generation of a climatology
+
+    Generates a .npy file of the map occurrency, a 2d map of integers, containing
+    the number of measurements found in all the daily files for each i,j indexes
+    That file will be used to generate the Sat maskfile
+    ''',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(   '--inputdir', '-i',
+                                type = str,
+                                required = True,
+                                help = '''ORIG sat directory, e.g. /gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/CCI_1km/DAILY/ORIG'''
+                                )
+    parser.add_argument(   '--var', '-v',
+                                type = str,
+                                required = True,
+                                help = '''CHL or KD490, the name of the NetCDF variable in ORIG files'''
+                                )
+    parser.add_argument(   '--outfile', '-o',
+                                type = str,
+                                required = True,
+                                help = '''CHL_map_occurency.npy'''
+                                )
+    return parser.parse_args()
+
+args = argument()
+
+
 from commons.Timelist import TimeList
-from commons.time_interval import TimeInterval
 import Sat.SatManager as Sat
 import numpy as np
+from commons.utils import addsep
 
-INPUTDIR="/gss/gss_work/DRES_OGS_BiGe/Observations/TIME_RAW_DATA/STATIC/SAT/KD490/DAILY/ORIG/"
-TI = TimeInterval("19500101","20500101","%Y%m%d")
-TL = TimeList.fromfilenames(TI, INPUTDIR,"*.nc",prefix='',dateformat='%Y%m%d')
+INPUTDIR=addsep(args.inputdir)
 
+
+TL = TimeList.fromfilenames(None, INPUTDIR,"*.nc",prefix='',dateformat='%Y%m%d')
 
 COUNT = np.zeros((1580,3308),np.int32)
 for ifile, filename in enumerate(TL.filelist):
-    print ifile
+    print "Working on file number ", ifile, " of ", len(TL.filelist)
     fillvalue=-999
-    A=Sat.readfromfile(filename,'KD490')
+    A=Sat.readfromfile(filename,args.var)
     ii=np.isnan(A)
     A[ii] = fillvalue
     filled = A==fillvalue
@@ -21,4 +53,4 @@ for ifile, filename in enumerate(TL.filelist):
     
 
 
-np.save('Kd_map_occurency',COUNT)
+np.save(args.outfile,COUNT)
