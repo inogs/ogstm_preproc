effccbf0958eb27d8336ab9932e7fa3668c7e80d
Giorgio Bolzon <gbolzon@inogs.it>
2021-03-12 12:59:55 +0100

    Adding dic_alk_profile_converter.py

---
+++ IC/gen_inputs_IC/dic_alk_profile_converter.py
@@ -0,0 +1,50 @@
+from commons.mask import Mask
+from commons.submask import SubMask
+from commons import density
+from basins import V2
+import numpy as np
+from commons import netcdf4
+import netCDF4
+
+IngvMask = Mask('/gpfs/work/IscrC_REBIOMED/NRT_EAS6/PREPROC/MASK/ogstm/meshmask_CMCCfor_ogstm.nc')
+TheMask = Mask('/gpfs/work/IscrC_REBIOMED/NRT_EAS6/PREPROC/MASK/ogstm/meshmask.nc')
+filename='/gpfs/scratch/userexternal/gbolzon0/V7C/T201901.nc'
+jpk, jpj, jpi = TheMask.shape
+JPK, JPJ, JPI = IngvMask.shape
+rho =density.get_density(filename, IngvMask)
+rho=rho[:jpk,:,JPI-jpi:]
+
+def dumpfile(outfile,M2d,var):
+    ncOUT = netCDF4.Dataset(outfile,'w')
+    ncOUT.createDimension('nsub',16)
+    ncOUT.createDimension('nav_lev',jpk)
+    ncvar=ncOUT.createVariable(var,'f',('nsub','nav_lev'))
+    ncvar[:]=M2d
+    ncOUT.close()
+RHO = np.zeros((17,jpk))
+
+for isub, sub in enumerate(V2.Pred):
+    print isub
+    S = SubMask(sub,maskobject=TheMask)    
+    for k in range(jpk):
+        submask = S.mask[k,:,:]
+        V=rho[k,:,:]
+        n = submask.sum()
+        if n> 0:
+            Conc = V[submask]
+            Weight=TheMask.area[submask]
+            Weight_sum      = Weight.sum()
+            Mass            = (Conc * Weight).sum()
+            Weighted_Mean   = Mass/Weight_sum
+            RHO[isub,k] = Weighted_Mean
+        else:
+            RHO[isub,k] = Weighted_Mean
+
+DIC=netcdf4.readfile("/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/IC/inputs/DIC_and_ALK/DIC.nc",'DIC')
+ALK=netcdf4.readfile("/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/IC/inputs/DIC_and_ALK/ALK.nc",'DIC')
+O3c = RHO[:16,:] * DIC * 12 /1000
+O3h = RHO[:16,:] * ALK /1000
+
+
+dumpfile('O3c.nc', O3c, 'O3c')
+dumpfile('O3h.nc', O3h, 'O3h')
