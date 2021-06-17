f5e65e923713b21555bef283823f8a1e6f46887f
Giorgio Bolzon <gbolzon@inogs.it>
2021-06-17 10:18:47 +0200

    Updating forcing_linker

--- setupModel/forcing_linker.sh
+++ setupModel/forcing_linker.sh
@@ -5,9 +5,17 @@
 # ln -s  assw_v4_1d_20151231_20160101_grid_T.nc T20160101-00:00:00.nc
 ######################################################################
 
-DIR=/gpfs/scratch/userexternal/plazzari/eas_v12/FORCINGS/UNZIPPED
-for I in `ls $DIR ` ; do
-  ogstm_name=${I:35:1}${I:21:8}-00:00:00.nc
-  ln -s ${DIR}/${I} $ogstm_name
+
+DIR=/gpfs/scratch/userexternal/gcoidess/TRANSITION_PREOPERATIONAL
+for I in `ls $DIR/mfs[12]*nc `; do
+   filename=`basename $I`
+   var=${filename:${#filename}-4:1}
+   date8=${filename:${#filename}-18:8}
+   ln -fs $I ${var}${date8}-12:00:00.nc
 done
+
+ln -fs T20190101-12:00:00.nc T20190101-00:00:00.nc
+ln -fs U20190101-12:00:00.nc U20190101-00:00:00.nc
+ln -fs V20190101-12:00:00.nc V20190101-00:00:00.nc
+ln -fs W20190101-12:00:00.nc W20190101-00:00:00.nc
 
