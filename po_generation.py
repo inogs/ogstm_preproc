f42e87ea4f361cb08988ebdea3c0752692340324
Giorgio Bolzon <gbolzon@inogs.it>
2021-09-03 16:18:55 +0200

    Po generation: - N5s is set to 75 mmol/m3 in order to preserve kTons balance - adding diagnostics

--- po_generation.py
+++ po_generation.py
@@ -1,10 +1,11 @@
 import numpy as np
 from commons import netcdf4
-from commons.Timelist import TimeList
+from commons.Timelist import TimeList, TimeInterval
 from commons.mask import Mask
 from commons.dataextractor import DataExtractor
 import seawater as sw
 import netCDF4
+from bclib.river import conversion
 
 
 def dump_file(filename,N,P,S,A,D,O,mask):
@@ -39,7 +40,7 @@ def dump_file(filename,N,P,S,A,D,O,mask):
 
 
 INPUTDIR="/gpfs/scratch/userexternal/gbolzon0/PO/TEST_01/wrkdir/MODEL/FORCINGS/"
-OUTDIR="/gpfs/scratch/userexternal/gbolzon0/P0O/ogstm_boundary_conditions/out/"
+OUTDIR="/gpfs/scratch/userexternal/gbolzon0/PO/ogstm_boundary_conditions/out/"
 
 riverinput="/gpfs/scratch/userexternal/gbolzon0/PO/runoff_1d_nomask_y2019.nc"
 
@@ -59,7 +60,7 @@ VARLIST=['O2o','N1p','N3n','N5s','O3c','O3h']
 RIVER_CONCENTRATION={'O2o':250  ,  #mmol/m3
                      'N1p':2.572,  #mmol/m3
                      'N3n':150  ,  #mmol/m3
-                     'N5s':150  ,  #mmol/m3
+                     'N5s':75   ,  #mmol/m3
                      'O3c':33225,  #  mg/m3
                      'O3h':2800 }  #mmol/m3
 
@@ -70,12 +71,15 @@ SALI = np.ones((10,),np.float32)*18
 PRES = np.ones((10,),np.float32)*CMCC_Mask.zlevels[0]
 
 cut =JPI - jpi
-
-TL = TimeList.fromfilenames(None, INPUTDIR, "T*.nc", prefix="T")
+TI = TimeInterval('2019','2020','%Y')
+TL = TimeList.fromfilenames(TI, INPUTDIR, "T*.nc", prefix="T")
+nFrames = TL.nTimes
 my_dtype=[(var, np.float32) for var in VARLIST]
 M2D = np.ones((jpj,JPI), dtype=my_dtype)
 PO  = np.ones((jpj,jpi), dtype=my_dtype)
+Total_Po = np.zeros((nFrames,), dtype=my_dtype)
 
+yearly_volume=0
 
 for iFrame, filename in enumerate(TL.filelist):
     outfile=OUTDIR + "PO__" + TL.Timelist[iFrame].strftime("%Y%m%d-%H:%M:%S") + ".nc"
@@ -87,8 +91,12 @@ for iFrame, filename in enumerate(TL.filelist):
     RHO    = sw.dens(SALI,T,PRES)
     
     Discharge = Runoff[ii]*CMCC_Mask.area[ii]/RHO  # m^3/s
+    daily_volume = Discharge.sum()*86400
+    yearly_volume += daily_volume
+
     
     for var in VARLIST:
+        Total_Po[iFrame][var] = daily_volume * RIVER_CONCENTRATION[var] # mmol/day
         M2D[var] = -1.0
         M2D[var][ii] = Discharge * RIVER_CONCENTRATION[var] / CMCC_Mask.area[ii]
         
@@ -96,4 +104,19 @@ for iFrame, filename in enumerate(TL.filelist):
         PO[var][~mask0] = 1.e+20
         
     dump_file(outfile, PO['N3n'], PO['N1p'], PO['N5s'], PO['O3h'], PO['O3c'], PO['O2o'], TheMask)
-    
+
+
+np.save('Total_po.npy',Total_Po)
+
+
+for var in VARLIST:
+    print "mmol/year", var, yearly_volume*RIVER_CONCENTRATION[var]
+    print "KTons/year", var, conversion(var)*yearly_volume*RIVER_CONCENTRATION[var]
+
+
+# KTons/year O2o 12.215648423583986
+# KTons/year N1p 3.8959123204367927
+# KTons/year N3n 102.61144675810547
+# KTons/year N5s 205.22289351621095
+# KTons/year O3c 1623.4596754943116
+# KTons/year O3h 136.81526234414062
