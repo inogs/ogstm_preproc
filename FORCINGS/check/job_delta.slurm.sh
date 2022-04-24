f20be2d67dec34bb3b45c98717d44a5de780a186
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2022-04-24 22:36:38 +0200

    Updating FORCINGS/check/job_delta.slurm.sh for g100

--- FORCINGS/check/job_delta.slurm.sh
+++ FORCINGS/check/job_delta.slurm.sh
@@ -1,29 +1,25 @@
 #! /bin/bash
 
-#SBATCH --job-name=test_txt
+#SBATCH --job-name=deltat
 #SBATCH -N2
-#SBATCH --ntasks-per-node=20
+#SBATCH --ntasks-per-node=30
 #SBATCH --time=02:00:00
-#SBATCH --mem=100gb
-#SBATCH --account=OGS20_PRACE_P
-#SBATCH --partition=gll_usr_prod
-#SBATCH --qos=gll_qos_dbg
+#SBATCH --mem=300gb
+#SBATCH --account=OGS21_PRACE_P
+#SBATCH --partition=g100_usr_prod
+#SBATCH --qos=g100_qos_dbg
 
 cd $SLURM_SUBMIT_DIR
 . ../../profile.inc
-module purge
-module load profile/base
-module load intel/pe-xe-2018--binary intelmpi/2018--binary
-module load autoload
-module load hdf5/1.8.18--intel--pe-xe-2018--binary netcdf/4.6.1--intel--pe-xe-2018--binary
-module load mpi4py/3.0.0--intelmpi--2018--binary
 
-source /gpfs/work/OGS20_PRACE_P/COPERNICUS/py_env_2.7.12/bin/activate
-export PYTHONPATH=$PYTHONPATH:/gpfs/work/OGS20_PRACE_P/COPERNICUS/bit.sea
+source /g100_work/OGS21_PRACE_P/COPERNICUS/sequence3.sh
 
-INPUTDIR=/gpfs/scratch/userexternal/gcoidess/FORCINGS_REA24/1993_transf/
-OUTPUTDIR=/gpfs/scratch/userexternal/gcoidess/FORCINGS_REA24/TXT/
-INGVMASK=/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/MASK/ogstm/meshmask_INGVfor_ogstm.nc
+unset I_MPI_PMI_LIBRARY
+
+
+INPUTDIR=/g100_scratch/userexternal/gbolzon0/V9C/2019/FORCINGS/
+OUTPUTDIR=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/FORCINGS/DELTAT
+INGVMASK=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/MASK/meshmask_CMCC.nc
 
 
 my_prex_or_die "mpirun python delta_t_from_uvw.py -i $INPUTDIR -o $OUTPUTDIR -m $INGVMASK"
