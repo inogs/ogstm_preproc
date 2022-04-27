20219e44c3c938ca095bd53f50808b291231ca61
Giorgio Bolzon <gbolzon@inogs.it>
2022-04-27 15:19:37 +0200

    setting general launcher.sh

--- BC/launcher.sh
+++ BC/launcher.sh
@@ -1,8 +1,19 @@
 #! /bin/bash
 
-MASKFILE=/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/MASK/ogstm/meshmask.nc
-INPUTDIR=/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/BC/inputs
-OUTPUTDIR=/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/BC/OUTPUT/ALLVARS
-RESTARTSDIR=/gpfs/work/IscrC_REBIOMED/REANALISI_24/PREPROC/IC/RST/
+MASKFILE=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/MASK/meshmask.nc
+INPUTBC=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/BC/inputs
 
-python atlantic_generator.py -i $INPUTDIR -o $OUTPUTDIR -m $MASKFILE --rst $RESTARTSDIR
+python write_VPnutrients.py -i $INPUTBC -m $MASKFILE
+python main.py
+
+RESTARTSDIR=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/IC/RST_2018
+OUTDIR=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/BC/out
+
+cd atlantic
+python atlantic_generator.py -i $INPUTBC -o $OUTDIR -m $MASKFILE --rst $RESTARTSDIR
+
+cd ..
+CMCC_MASK=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/MASK/meshmask_CMCC.nc
+FORCINGS=/g100_scratch/userexternal/gbolzon0/V9C/2019/FORCINGS/
+python Po/online/po_generation -i $FORCINGS -o $OUTDIR -M $CMCC_MASK -m $MASKFILE
+
