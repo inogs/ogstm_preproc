fe9e6e230bbec22f17f1118af48a4cba46d59c5f
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2022-09-28 09:21:45 +0200

    Renaming paths

--- BC/launcher.sh
+++ BC/launcher.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-MASKFILE=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/MASK/meshmask.nc
-INPUTBC=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/BC/inputs
+MASKFILE=/g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/MASK/meshmask.nc
+INPUTBC=/g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/BC/inputs
 
 python write_VPnutrients.py -i $INPUTBC -m $MASKFILE
 python main.py
 
-RESTARTSDIR=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/IC/RST_2018
-OUTDIR=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/BC/out
+RESTARTSDIR=/g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/IC/RST_2018
+OUTDIR=/g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/BC/out
 
 cd atlantic
 python atlantic_generator.py -i $INPUTBC -o $OUTDIR -m $MASKFILE --rst $RESTARTSDIR
 
 cd ..
-CMCC_MASK=/g100_work/OGS_prod100/OPA/V9C/RUNS_SETUP/PREPROC/MASK/meshmask_CMCC.nc
+CMCC_MASK=/g100_work/OGS_devC/V9C/RUNS_SETUP/PREPROC/MASK/meshmask_CMCC.nc
 FORCINGS=/g100_scratch/userexternal/gbolzon0/V9C/2019/FORCINGS/
 python Po/online/po_generation -i $FORCINGS -o $OUTDIR -M $CMCC_MASK -m $MASKFILE
 
