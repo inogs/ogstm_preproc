0c4220c6cfce0b768217d0fc107bc73c742315f9
Giorgio Bolzon <gbolzon@inogs.it>
2023-07-11 14:30:27 +0200

    Working state of energy 1. Saving netcdf outputs 2. KE_ratio is logarithmic and takes in account vmask and vmask False 3. take in account negative values returned by sw.bfrq()

--- FORCINGS/check/energy.py
+++ FORCINGS/check/energy.py
@@ -30,6 +30,7 @@ import seawater as sw
 from surf import surfaces
 from commons.layer import Layer
 from layer_integral.mapbuilder import MapBuilder
+from commons import netcdf4
 
 try:
     from mpi4py import MPI
@@ -45,8 +46,11 @@ OUTPUTDIR=addsep(args.outdir)
 TheMask=Mask(args.maskfile)
 
 jpk, jpj, jpi = TheMask.shape
-izlev=TheMask.getDepthIndex(200) + 1 #350
-M=np.zeros_like(TheMask.mask)*np.nan
+izlev=TheMask.getDepthIndex(200) + 5 #350
+M=np.zeros((jpk,jpj,jpi),np.float32)*np.nan
+BVF=np.zeros((jpk,jpj,jpi),np.float32)
+mask = TheMask.mask_at_level(0)
+
 for ii in range(jpi):
     for jj in range(jpj):
         M[:,jj,ii]=TheMask.zlevels
@@ -92,9 +96,37 @@ for iframe in FRAMES[rank::nranks]:
     V[V>1.e+19]=eps
     W[W>1.e+19]=eps
     KE_ratio = W**2/(U**2 + V**2)
-# 
-    BVF, _, p_ave = sw.bfrq(S,T,P)
-    De = DataExtractor(TheMask,rawdata=BVF*M)
+
+    ii=(U==eps) & (V==eps) # taking in account umask and vmask False
+    KE_ratio[ii] = 0
+
+    De = DataExtractor(TheMask,rawdata=KE_ratio)
+    KE_ratio_2d = MapBuilder.get_layer_integral(De,layer200)
+    lmask = (KE_ratio_2d>0) & mask
+    KE_ratio_2d[lmask] = np.log10(KE_ratio_2d[lmask])
+    KE_ratio_2d[~lmask] = 1.e+20
+
+
+    KE_total = 0.5*(U**2 + V**2 + W**2)
+    De = DataExtractor(TheMask,rawdata=KE_total)
+    KE_total_2d = MapBuilder.get_layer_integral(De,layer200)
+
+    BVF[:izlev-1,:], _, p_ave = sw.bfrq(S[:izlev,:],T[:izlev,:],P)
+    BVF[BVF<0]=0 # because sw.bfrw returns a negative value when S=0, T=0 is encountered as first land point
+    De = DataExtractor(TheMask,rawdata=BVF*M)
     Stratification_index = MapBuilder.get_layer_integral(De, layer200)
 
+    Stratification_index[~mask] = 1.e+20
+    KE_total_2d[~mask] = 1.e+20
+    mld[~mask] = 1.e+20
+    Eddy_diff_200[~mask] = 1.e+20
+    Eddy_diff_500[~mask] = 1.e+20
+
+    outfile=OUTPUTDIR + "metrics." + timestr + ".nc"
+    netcdf4.write_2d_file(KE_ratio_2d         ,'KE_ratio', outfile, TheMask, compression=True)
+    netcdf4.write_2d_file(KE_total_2d         ,'KE_total', outfile, TheMask, compression=True)
+    netcdf4.write_2d_file(Stratification_index,'stratification_index', outfile, TheMask, compression=True)
+    netcdf4.write_2d_file(mld                 ,'mld' , outfile,TheMask, compression=True)
+    netcdf4.write_2d_file(Eddy_diff_200       ,'Evd_200', outfile, TheMask, compression=True)
+    netcdf4.write_2d_file(Eddy_diff_500       ,'Evd_500', outfile, TheMask, compression=True)
 
