a08668fe0f21addd8050ae54ccc6da29402ef6b9
Giorgio Bolzon <gbolzon@inogs.it>
2023-10-19 09:37:27 +0200

    Bug fixes and dumping on date directory

--- FORCINGS/check/Kz_local_impact.py
+++ FORCINGS/check/Kz_local_impact.py
@@ -4,11 +4,12 @@ matplotlib.use('Qt5Agg')
 import pylab as pl
 from commons.mask import Mask
 from commons.dataextractor import DataExtractor
+import os
 
 TheMask=Mask('/g100_work/OGS_devC/Benchmark/SETUP/PREPROC/MASK/meshmask.nc')
 DIR1="/g100_scratch/userexternal/gbolzon0/V10C/TEST_FORCINGS_24h/wrkdir/MODEL/AVE_FREQ_1/"
 DIR2="/g100_scratch/userexternal/gbolzon0/V10C/run1.1/wrkdir/MODEL/AVE_FREQ_1/"
-OUTDIR=""
+OUTDIR="/g100_work/OGS_devC/Benchmark/pub/gbolzon/eas8/24h/correction_effect"
 
 
 def plot_profile(i,j, outfile):
@@ -25,12 +26,14 @@ def plot_profile(i,j, outfile):
     ax.set_ylabel('z (m)')
     fig.suptitle("%s  i=%d, j=%d" %(var,j,i))
     fig.savefig(outfile)
+    pl.close(fig)
 
 
 date="20190413"
+os.system('mkdir -p %s/%s' %(OUTDIR,date))
 
-I = [410,904,370,663]
-J = [181,129,195,214]
+I_survey_points = [410,904,370,663]
+J_survey_points = [181,129,195,214]
 
 i,j = 410, 181
 i,j = 904, 129
@@ -38,7 +41,8 @@ i,j = 370, 195
 i,j = 663, 214 # la correzione non ha preso in z<150
 for var in ["N1p", "N3n", "O2o"]:
 
-    outfile="%s/worstcase_%s.%s.png" %(OUTDIR,date,var)
+    outfile="%s/%s/worstcase_%s.%s.png" %(OUTDIR,date,date,var)
+    print(outfile)
 
     filename="ave." + date + "-12:00:00." + var + ".nc"
     V_orig=DataExtractor(TheMask, DIR1+filename, var ).values
@@ -50,10 +54,11 @@ for var in ["N1p", "N3n", "O2o"]:
     k,j,i = K[0],J[0],I[0]
 
     plot_profile(i, j, outfile)
-    for k in range(len(I)):
-        i=I[k]
-        j=J[k]
-        outfile="%s/%d_%d_%s.%s.png" %(OUTDIR,i,j,date,var)
+    for k in range(len(I_survey_points)):
+        i=I_survey_points[k]
+        j=J_survey_points[k]
+        outfile="%s/%s/%d_%d_%s.%s.png" %(OUTDIR,date,i,j,date,var)
+        print(outfile)
         plot_profile(i, j, outfile)
     # k = B[j,i]
     # fig,ax=pl.subplots()
