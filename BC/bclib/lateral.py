4942f95c1fb128aee9993c449e3ce0cb9e813436
ginevrar <ginevra.rosati@gmail.com>
2025-01-13 12:36:57 +0100

    Debugging for Hg vars in GIB

--- BC/bclib/lateral.py
+++ BC/bclib/lateral.py
@@ -25,6 +25,8 @@ class lateral_bc:
 
     def _convert_information(self,mask):
         jpt = len(self.season)
+        print('self.season',self.season)
+        print('--------------------')
         jpk, jpj, jpi = mask.shape
         size_nutrients = (jpt,jpk,jpj, jpi)
         self.phos = np.zeros(size_nutrients, np.float64)
@@ -35,12 +37,22 @@ class lateral_bc:
         self.alk  = np.zeros(size_nutrients, np.float64)
         self.Hg0  = np.zeros(size_nutrients, np.float64)
         self.Hg2  = np.zeros(size_nutrients, np.float64)
-        self.MMHg  = np.zeros(size_nutrients, np.float64)
-        self.DMHg  = np.zeros(size_nutrients, np.float64)
+        self.MHg  = np.zeros(size_nutrients, np.float64)
+        self.DHg  = np.zeros(size_nutrients, np.float64)
+        self.P1h  = np.zeros(size_nutrients, np.float64)
+        self.P2h  = np.zeros(size_nutrients, np.float64)
+        self.P3h  = np.zeros(size_nutrients, np.float64)
+        self.P4h  = np.zeros(size_nutrients, np.float64)
+        self.Z6h  = np.zeros(size_nutrients, np.float64)
+        self.Z5h  = np.zeros(size_nutrients, np.float64)
+        self.Z4h  = np.zeros(size_nutrients, np.float64)
+        self.Z3h  = np.zeros(size_nutrients, np.float64)
         #tmask4d   = np.zeros(size_nutrients, np.bool)
         tmask4d   = np.zeros(size_nutrients, bool)
         nav_lev = mask.zlevels
         n_lev = jpk
+        print('n_lev',n_lev)
+        #n_lev2 = jpk
         vp_phos = np.zeros((jpt,n_lev));
         vp_ntra = np.zeros((jpt,n_lev));
         vp_sica = np.zeros((jpt,n_lev));
@@ -49,8 +61,16 @@ class lateral_bc:
         vp_alk  = np.zeros((jpt,n_lev));
         vp_Hg0  = np.zeros((jpt,n_lev));
         vp_Hg2  = np.zeros((jpt,n_lev));
-        vp_MMHg = np.zeros((jpt,n_lev));
-        vp_DMHg = np.zeros((jpt,n_lev));
+        vp_MHg = np.zeros((jpt,n_lev));
+        vp_DHg = np.zeros((jpt,n_lev));
+        vp_P1h = np.zeros((jpt,n_lev));
+        vp_P2h = np.zeros((jpt,n_lev));
+        vp_P3h = np.zeros((jpt,n_lev));
+        vp_P4h = np.zeros((jpt,n_lev));
+        vp_Z6h = np.zeros((jpt,n_lev));
+        vp_Z5h = np.zeros((jpt,n_lev));
+        vp_Z4h = np.zeros((jpt,n_lev));
+        vp_Z3h = np.zeros((jpt,n_lev));
 
         nav_lev_in=np.concatenate(([0],self.lev1,[5500]))
 
@@ -58,33 +78,50 @@ class lateral_bc:
         vp_ntra_in=np.column_stack((self.N3n[:,0], self.N3n, self.N3n[:,-2]))
         vp_sica_in=np.column_stack((self.N5s[:,0], self.N5s, self.N5s[:,-2]))
         vp_dox_in =np.column_stack((self.O2o[:,0], self.O2o, self.O2o[:,-2]))
-#       vp_Hg0_in =np.column_stack((self.Hg0[:,0], self.Hg0, self.Hg0[:,-2]))
+#######  vp_Hg0_in =np.column_stack((self.Hg0[:,0], self.Hg0, self.Hg0[:,-2]))
+#### end of two dimensional vars - 1D vars
+
         end = len(self.ALK)
+        print('END',end)
         nav_lev_in2 = np.append([0],self.lev2.T)
         nav_lev_in2 = np.append(nav_lev_in2,[5500])
-
+        print('NAVin2',nav_lev_in2 ) 
         vp_dic_in = np.append(self.DIC[1],self.DIC[:].T)
         vp_dic_in = np.append(vp_dic_in,self.DIC[end-1])
 
         vp_alk_in = np.append(self.ALK[1],self.ALK[:].T)
         vp_alk_in = np.append(vp_alk_in,self.ALK[end-1]);
 
-        end2=len(self.Hg0)
+        print('vp_alk_in0',vp_alk_in)
+        print('self.ALK[1]',self.ALK[1])
+        print('self.ALK[:].T',self.ALK[:].T)
+
+        end2=len(self.Hg0[0])
         print('end for Hg0',end2)
+        print('self.Hg0[0][1][0]',self.Hg0[0][1][0])
+        print('self.Hg0[0].T',self.Hg0[0][1][0].T)
         nav_lev_in3 = np.append([0],self.lev3.T)
         nav_lev_in3 = np.append(nav_lev_in3,[5500])
+   #     nav_lev_in3=np.concatenate(([0],self.lev3,[5500]))
+        print('NAVin3',nav_lev_in3) 
 
-        vp_Hg0_in = np.append(self.Hg0[0],self.Hg0[:].T)
-        vp_Hg0_in = np.append(vp_Hg0_in,self.Hg0[end2-1])
+        vp_Hg0_in = np.append(self.Hg0[0][1],self.Hg0[:].T)
+        print('vp_Hg0_in0',vp_Hg0_in)
+        print('self.Hg0[0]',self.Hg0)
+        print('self.Hg0[0].T',self.Hg0[0][1].T)
+        vp_Hg0_in = np.append(vp_Hg0_in,self.Hg0[0][end2-1])
+     #   print('vp_Hg0_in1',vp_Hg0_in)
+     #   print('self.Hg0[0]',self.Hg0[0])
+     #   print('self.Hg0[0].T',self.Hg0[0].T)
 
         vp_Hg2_in = np.append(self.Hg2[0],self.Hg2[:].T)
         vp_Hg2_in = np.append(vp_Hg2_in,self.Hg2[end2-1])
 
-        vp_MMHg_in = np.append(self.MMHg[0],self.MMHg[:].T)
-        vp_MMHg_in = np.append(vp_MMHg_in,self.MMHg[end2-1])
+        vp_MHg_in = np.append(self.MHg[0],self.MHg[:].T)
+        vp_MHg_in = np.append(vp_MHg_in,self.MHg[end2-1])
 
-        vp_DMHg_in = np.append(self.DMHg[0],self.DMHg[:].T)
-        vp_DMHg_in = np.append(vp_DMHg_in,self.DMHg[end2-1])
+        vp_DHg_in = np.append(self.DHg[0],self.DHg[:].T)
+        vp_DHg_in = np.append(vp_DHg_in,self.DHg[end2-1])
 
         print('CHECK WITH GIORGIO')
 
@@ -102,14 +139,15 @@ class lateral_bc:
             jj=~ np.isnan(vp_alk_in[:]) | ( vp_alk_in[:] < 1e19)
             vp_alk[i,:]  = np.interp(nav_lev,nav_lev_in2[jj],vp_alk_in[jj])
 
+            print('vp_Hg0_in[:]',vp_Hg0_in[:].shape)
             jj=~ np.isnan(vp_Hg0_in[:]) | ( vp_Hg0_in[:] < 1e19)
             vp_Hg0[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_Hg0_in[jj])
             jj=~ np.isnan(vp_Hg2_in[:]) | ( vp_Hg2_in[:] < 1e19)
             vp_Hg2[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_Hg2_in[jj])
-            jj=~ np.isnan(vp_MMHg_in[:]) | ( vp_MMHg_in[:] < 1e19)
-            vp_MMHg[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_MMHg_in[jj])
-            jj=~ np.isnan(vp_DMHg_in[:]) | ( vp_DMHg_in[:] < 1e19)
-            vp_DMHg[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_DMHg_in[jj])
+            jj=~ np.isnan(vp_MHg_in[:]) | ( vp_MHg_in[:] < 1e19)
+            vp_MHg[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_MHg_in[jj])
+            jj=~ np.isnan(vp_DHg_in[:]) | ( vp_DHg_in[:] < 1e19)
+            vp_DHg[i,:]  = np.interp(nav_lev,nav_lev_in3[jj],vp_DHg_in[jj])
 
 #
 # %Loop on time seasonal
@@ -123,8 +161,8 @@ class lateral_bc:
                     self.alk[ jt,jk,:,:] = vp_alk[ jt,jk]
                     self.Hg0[ jt,jk,:,:] = vp_Hg0[ jt,jk]
                     self.Hg2[ jt,jk,:,:] = vp_Hg2[ jt,jk]
-                    self.MMHg[ jt,jk,:,:] = vp_MMHg[ jt,jk]
-                    self.DMHg[ jt,jk,:,:] = vp_DMHg[ jt,jk]
+                    self.MHg[ jt,jk,:,:] = vp_MHg[ jt,jk]
+                    self.DHg[ jt,jk,:,:] = vp_DHg[ jt,jk]
 
 
             for jt in range(jpt): tmask4d[jt,:,:,:]=mask.mask
@@ -137,5 +175,5 @@ class lateral_bc:
             self.alk[ ~tmask4d]=1.e+20
             self.Hg0[ ~tmask4d]=1.e+20
             self.Hg2[ ~tmask4d]=1.e+20
-            self.MMHg[ ~tmask4d]=1.e+20
-            self.DMHg[ ~tmask4d]=1.e+20
+            self.MHg[ ~tmask4d]=1.e+20
+            self.DHg[ ~tmask4d]=1.e+20
