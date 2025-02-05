2cb625efdf88817f0832e3c406630eae730b3e73
Giorgio Bolzon <gbolzon@ogs.it>
2025-02-05 13:53:01 +0100

    Updating downloader.sh from ftp to MDS

--- Download/downloader.sh
+++ Download/downloader.sh
@@ -1,28 +1,32 @@
 #! /bin/bash
 
-#SBATCH --job-name=WGET
+#SBATCH --job-name=MDS
 #SBATCH -N1 -n 1
 #SBATCH --time=4:00:00
-#SBATCH --account=OGS_dev
+#SBATCH --account=OGS23_PRACE_IT
 #SBATCH --partition=gll_all_serial
+
 
 cd $SLURM_SUBMIT_DIR
 
 . ../profile.inc
 
-PRODUCT=OCEANCOLOUR_MED_BGC_L3_MY_009_143
+ORIGDIR=
+mkdir -o $ORIGDIR
 
-#cmems_obs-oc_med_bgc-optics_my_l3-multi-1km_P1D
 DATASET_LIST="
-cmems_obs-oc_med_bgc-reflectance_my_l3-olci-300m_P1D
 cmems_obs-oc_med_bgc-plankton_my_l3-multi-1km_P1D
 cmems_obs-oc_med_bgc-transp_my_l3-multi-1km_P1D
-cmems_obs-oc_med_bgc-plankton_my_l3-olci-300m_P1D
-cmems_obs-oc_med_bgc-transp_my_l3-olci-300m_P1D
-cmems_obs-oc_med_bgc-reflectance_my_l3-multi-1km_P1D"
+"
+#cmems_obs-oc_med_bgc-plankton_my_l3-olci-300m_P1D
+#cmems_obs-oc_med_bgc-transp_my_l3-olci-300m_P1D
+#cmems_obs-oc_med_bgc-reflectance_my_l3-multi-1km_P1D"
 
-for dataset in $DATASET_LIST; do 
-
-   my_prex "wget --ftp-user=MED_OGS_TRIESTE_IT --ftp-password=NEdifupa -m ftp://my.cmems-du.eu/Core/${PRODUCT}/${dataset}"
-
+COMMONS=" -nd -s files --force-download --show-outputnames --overwrite --disable-progress-bar "
+for year in $(seq 1990 2023 ) ; do
+    for dataset in $DATASET_LIST; do
+        my_prex "copernicusmarine get $COMMONS -o ${ORIGDIR}  -i ${dataset}  --filter *${year}/*"
+    done
 done
+
+
