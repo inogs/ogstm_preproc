72db67533476f808ff1a75bb6e53de41720a6fe7
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2025-02-08 19:15:37 +0100

    typos

--- Download/downloader.sh
+++ Download/downloader.sh
@@ -4,7 +4,7 @@
 #SBATCH -N1 -n 1
 #SBATCH --time=4:00:00
 #SBATCH --account=OGS23_PRACE_IT
-#SBATCH --partition=gll_all_serial
+#SBATCH --partition=g100_all_serial
 
 
 cd $SLURM_SUBMIT_DIR
@@ -12,7 +12,7 @@ cd $SLURM_SUBMIT_DIR
 . ../profile.inc
 
 ORIGDIR=
-mkdir -o $ORIGDIR
+mkdir -p $ORIGDIR
 
 DATASET_LIST="
 cmems_obs-oc_med_bgc-plankton_my_l3-multi-1km_P1D
@@ -23,7 +23,7 @@ cmems_obs-oc_med_bgc-transp_my_l3-multi-1km_P1D
 #cmems_obs-oc_med_bgc-reflectance_my_l3-multi-1km_P1D"
 
 COMMONS=" -nd -s files --force-download --show-outputnames --overwrite --disable-progress-bar "
-for year in $(seq 1990 2023 ) ; do
+for year in $(seq 1999 2023 ) ; do
     for dataset in $DATASET_LIST; do
         my_prex "copernicusmarine get $COMMONS -o ${ORIGDIR}  -i ${dataset}  --filter *${year}/*"
     done
