1109bb93477507551b6b50754a6b1fa957261068
Giovanni Galli <ggalli00@login07.leonardo.local>
2026-01-22 14:18:32 +0100

    degrade_restart.py now does also generic files used for nudging (e.g. R3l), just change yaml file: python degrade_restart.py -y degrade_3Dvar.yaml

--- FORCINGS/degradation/degrade_restart.py
+++ FORCINGS/degradation/degrade_restart.py
@@ -13,13 +13,18 @@ from bitsea.commons.mask import Mask
 #from previous.restart_interpolator import RSTwriter
 
 '''
-degrades the horizontal resolution of BFM restarts by an integer value
-joins boxes of ndeg x ndeg cells, does volume weighted mean.
+degrades the horizontal resolution of some BFM 3D variable
+by an integer value. joins boxes of ndeg x ndeg cells, 
+does volume weighted mean.
 needs both original and degraded-resolution meshmask 
 (run degrade mesh.py before)
 
+THIS IS UGLY!
+it is basically the same as degrade_restart.py
+using it to degrade the R3l nudging files
+
 usage: 
-python degrade_mesh.py -y degrade_mesh.yaml
+python degrade_3Dvar.py -y degrade_3Dvar.yaml
 
 yaml file example:
 mesh_in: '/path/to/meshmask_original.nc'
@@ -28,6 +33,8 @@ indir: '/path/to/restarts/rst.nc' #also with wildcards '*'
 outdir: '/path/to/output_dir/'
 infile: 'RST.*-00:00:00.__VNAME__.nc' #__VNAME__ gets replaced and is needed to read the variable
 ndeg: 6 #size of boxes for resolution degradation
+isrst: True #are we degrading restarts (True) or other files (False)
+vname: None #only if degrading other files, specify variable name
 '''
 
 def argument():
@@ -37,7 +44,6 @@ def argument():
                         required=True,
                         help='file with all parameters')
     return parser.parse_args()
-
 
 def get_Volume(M):
     '''
@@ -50,22 +56,28 @@ def get_Volume(M):
     return V0
 
 
-def load_xpnd_rst(infile, vname, mask:Mask, ndeg=1):
+def load_xpnd_rst(infile, vname, mask:Mask, ndeg=1, isrst=True):
     '''
     loads  the data array from a variable in the restart and expands it in 'edge' mode
     Arguments:
         infile : path to restart file
         vname : bfm variable name
         mask : Mask object of the fine mesh
+        isrst : if working on restarts, adds 'TRN' in front of var name
 
     Returns:
     RST : xarray DataArray, with 3D variable already expanded
          in 'edge' mode, nan 
     '''
-    vname = "TRN"+vname
+    if isrst:
+        vname = "TRN"+vname
     with xr.open_dataset(infile) as D:
         A = D[vname]
-        A.values[0,:][~mask.mask] = np.nan
+        if isrst: #restarts have singleton time
+            A.values[0,:][~mask.mask] = np.nan
+        else: #R3l files have (depth, longitude, latitude)
+            A = A.rename({'longitude':'x', 'latitude':'y', 'depth':'z'})
+            A.values[:][~mask.mask] = np.nan
         RST = dm.xpnd_wrap(A, 'edge', ndeg)
     return RST
 
@@ -91,29 +103,40 @@ def degrade_bgc(DI, V0, Maskout:Mask, ndeg=1):
 def get_flist(Params):
     indir = Params['indir']
     infile = Params['infile']
+    isrst = Params['isrst']
     flist = glob(indir + infile.replace('__VNAME__', '*'))
-    itrbl = [(ff.split('.')[-2], Path(ff)) for ff in flist]
+    if isrst:
+        itrbl = [(ff.split('.')[-2], Path(ff)) for ff in flist]
+    else:
+        vname = Params['vname']
+        itrbl = [(vname, Path(ff)) for ff in flist]
     return itrbl
 
-def RSTwriter(outfile, var, rst, TheMask):
+def RSTwriter(outfile, var, rst, TheMask, isrst=True):
     rst[~TheMask.mask] = 1.e+20
     jpk, jpj, jpi = TheMask.shape
     ncOUT=netCDF4.Dataset(outfile,"w", format="NETCDF4")
     ncOUT.createDimension('x',jpi);
     ncOUT.createDimension('y',jpj);
     ncOUT.createDimension('z',jpk);
-    ncOUT.createDimension('time',1)
+    if isrst:
+        ncOUT.createDimension('time',1)
 
-    TRN   = 'TRN' + var;
+    if isrst:
+        var   = 'TRN' + var;
     ncvar = ncOUT.createVariable('nav_lon' ,'d',('y','x')           ); ncvar[:] = TheMask.xlevels
     ncvar = ncOUT.createVariable('nav_lat' ,'d',('y','x')           ); ncvar[:] = TheMask.ylevels
     ncvar = ncOUT.createVariable('nav_lev' ,'d',('z')               ); ncvar[:] = TheMask.zlevels
-    ncvar = ncOUT.createVariable('time'    ,'d',('time',)           ); ncvar    = 1.;
-    ncvar = ncOUT.createVariable(TRN       ,'d',('time','z','y','x') ); ncvar[:] = rst;
+    if isrst:
+        ncvar = ncOUT.createVariable('time'    ,'d',('time',)           ); ncvar    = 1.;
+        ncvar = ncOUT.createVariable(var       ,'d',('time','z','y','x') ); ncvar[:] = rst;
+    else:
+        ncvar = ncOUT.createVariable(var       ,'d',('z','y','x') ); ncvar[:] = rst;
 
-    setattr(ncOUT.variables[TRN]   ,'missing_value',1.e+20                             );
-    setattr(ncOUT.variables['time'],'Units'        ,'seconds since 1582-10-15 00:00:00');
-    setattr(ncOUT                  ,'TimeString'   ,'20010101-00:00:00');
+    setattr(ncOUT.variables[var]   ,'missing_value',1.e+20                             );
+    if isrst:
+        setattr(ncOUT.variables['time'],'Units'        ,'seconds since 1582-10-15 00:00:00');
+        setattr(ncOUT                  ,'TimeString'   ,'20010101-00:00:00');
     ncOUT.close()
     return
 
@@ -134,6 +157,7 @@ if __name__=='__main__':
     mesh_out = Path(Params['mesh_out'])
     outdir = Path(Params['outdir'])
     ndeg = Params['ndeg']
+    isrst = Params['isrst']
 
     M = dm.load_mesh(mesh_in, ndeg)
     V0 = get_Volume(M)
@@ -147,7 +171,7 @@ if __name__=='__main__':
         outfile = outdir / fname.name
         print(f'degrado: {vname}')
         # 
-        DI = load_xpnd_rst(fname, vname, Mask_in,ndeg)
+        DI = load_xpnd_rst(fname, vname, Mask_in, ndeg, isrst)
         Dd = degrade_bgc(DI, V0, Maskout, ndeg)
 
         print("eccolo")
@@ -155,5 +179,5 @@ if __name__=='__main__':
         #v[~Maskout.mask] = 1.0e20
         waterpoints=v[Maskout.mask]
         print(f' post-degradation min/max/nans/ {waterpoints.min()}, {waterpoints.max()}', np.sum(np.isnan(waterpoints)))
-        RSTwriter(outfile, vname, Dd.values[0,:], Maskout)
+        RSTwriter(outfile, vname, Dd.values[0,:], Maskout, isrst)
 
