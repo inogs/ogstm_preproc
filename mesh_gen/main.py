09b0c6b47f86b6594aa6a7e90d9363d10dc5903c
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2026-01-28 18:28:07 +0100

    creating 125 levels meshmask

--- mesh_gen/main.py
+++ mesh_gen/main.py
@@ -2,7 +2,7 @@ import create_meshmask_nc as c_mask
 
 infile  = '/g100_work/OGS_test2528/camadio/RUN_RIVER_INLETS/MASK/ORIG_CMCC/mesh_mask.nc'
 
-outfile = 'prova_full.nc'
+outfile = 'meshmask_CMCC.nc'
 
 M       = c_mask.OrigMask(infile)
 lon_cut = 0
@@ -14,7 +14,7 @@ c_mask.create_meshmask_nc(M,outfile,lon_cut,depth_cut,Biscay_land)
 outfile = 'meshmask.nc'
 
 lon_cut = M.getCutLocation(-8.875)
-depth_cut=11
+depth_cut=16
 Biscay_land = True
 c_mask.create_meshmask_nc(M,outfile,lon_cut,depth_cut,Biscay_land)
 
