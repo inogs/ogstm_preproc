e622cdfaba6e1ba9faa4ed266100495f7f4912e8
maverick-7487 <93658675+maverick-7487@users.noreply.github.com>
2026-03-03 18:04:53 +0100

    minor changes to tagliafile_xarray.py

--- FORCINGS/tagliafile_xarray.py
+++ FORCINGS/tagliafile_xarray.py
@@ -37,30 +37,30 @@ OUTPUTDIR=addsep(args.outputdir)
 filelist=glob.glob(INPUTDIR + "*nc")
 filelist.sort()
 
+coordinates_without_fillvalue = ["nav_lat", "nav_lon",
+                "deptht", "deptht_bounds",
+                "time_instant", "time_instant_bounds",
+                "time_counter", "time_counter_bounds", 
+                "time_centered", "time_centered_bounds"]
 for filename in filelist[rank::nranks]:
-    ds_file = xr.open_dataset(filename)
-    print("rank %d executes cut of %s" % (rank, filename), flush=True)
-    for it in range(4):
-        slice_name = str(ds_file.isel(time_counter=it).time_counter.values)
-        slice_name = slice_name.split('.')[0]
-        slice_time = slice_name.split('T')[1]
-        slice_date = slice_name.split('T')[0].replace('-', '')
-        outputfile = OUTPUTDIR + 'T' + slice_date + "-" + slice_time + ".nc"
-        ds_slice = ds_file.isel(time_counter=slice(it, it+1))
-        for var in ds_slice.variables:
-            if var in ["nav_lat", 
-            "nav_lon",
-            "deptht", "deptht_bounds",
-            "time_instant", "time_instant_bounds",
-            "time_counter", "time_counter_bounds", 
-            "time_centered", "time_centered_bounds"]:
-                ds_slice[var].encoding['_FillValue'] = None
-        # for var in ds_slice.variables:
-        #     print(f"Variable: {var}, Encoding: {ds_slice[var].encoding['_FillValue'] if '_FillValue' in ds_slice[var].encoding else 'None'  }")
-        ds_slice.to_netcdf(outputfile, mode="w", format="NETCDF4")
-        ds_slice.close()
-        print("rank %d generates %s" % (rank, outputfile), flush=True)
-    ds_file.close()
+    with xr.open_dataset(filename) as ds_file:
+        print("rank %d executes cut of %s" % (rank, filename), flush=True)
+        for it in range(4):
+            slice_name = str(ds_file.isel(time_counter=it).time_counter.values)
+            slice_name = slice_name.split('.')[0]
+            slice_time = slice_name.split('T')[1]
+            slice_date = slice_name.split('T')[0].replace('-', '')
+            outputfile = OUTPUTDIR + 'T' + slice_date + "-" + slice_time + ".nc"
+            ds_slice = ds_file.isel(time_counter=slice(it, it+1))
+            for var in ds_slice.variables:
+                if var in coordinates_without_fillvalue:
+                    ds_slice[var].encoding['_FillValue'] = None
+            # for var in ds_slice.variables:
+            #     print(f"Variable: {var}, Encoding: {ds_slice[var].encoding['_FillValue'] if '_FillValue' in ds_slice[var].encoding else 'None'  }")
+            ds_slice.to_netcdf(outputfile, mode="w", format="NETCDF4")
+            ds_slice.close()
+            print("rank %d generates %s" % (rank, outputfile), flush=True)
+
 
     
     
