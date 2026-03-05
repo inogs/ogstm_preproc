b03d5f40105a022fcba474f654b11c25a816439d
Giorgio Bolzon <gbolzon@ogs.trieste.it>
2026-03-05 14:34:40 +0100

    Using path objects

--- FORCINGS/tagliafile_xarray.py
+++ FORCINGS/tagliafile_xarray.py
@@ -1,15 +1,16 @@
+from bitsea.utilities.argparse_types import existing_dir_path
 import argparse
 from datetime import datetime
 
 def argument():
     parser = argparse.ArgumentParser(description = 'Executes gzip in parallel')
     parser.add_argument(   '--inputdir', '-i',
-                                type = str,
+                                type = existing_dir_path,
                                 required = True,
                                 help = 'The directory containing uncompressed files')
 
     parser.add_argument(   '--outputdir', '-o',
-                                type = str,
+                                type = existing_dir_path,
                                 required = True,
                                 help = 'The directory where you want to dump compressed files')
     return parser.parse_args()
@@ -19,7 +20,7 @@ args = argument()
 import glob
 import os
 import xarray as xr
-from bitsea.commons.utils import addsep
+
 try:
     from mpi4py import MPI
     comm  = MPI.COMM_WORLD
@@ -32,18 +33,11 @@ except:
     isParallel = False
 
 
-INPUTDIR =addsep(args.inputdir) 
-OUTPUTDIR=addsep(args.outputdir)
+INPUTDIR = args.inputdir
+OUTPUTDIR = args.outputdir
 
-# Crea la directory output se non esiste (solo rank 0)
-if rank == 0 and not os.path.exists(OUTPUTDIR.rstrip('/')):
-    os.makedirs(OUTPUTDIR.rstrip('/'), exist_ok=True)
-    print(f"Created output directory: {OUTPUTDIR}", flush=True)
 
-if isParallel:
-    comm.Barrier()  # Sincronizza prima di iniziare
-
-filelist=glob.glob(INPUTDIR + "*-12:00:00.nc")
+filelist=[f for f in INPUTDIR.glob("*-12:00:00.nc") ]
 filelist.sort()
 
 coordinates_without_fillvalue = ["nav_lat", "nav_lon",
@@ -52,8 +46,8 @@ coordinates_without_fillvalue = ["nav_lat", "nav_lon",
                 "time_counter", "time_counter_bounds", 
                 "time_centered", "time_centered_bounds"]
 
-dt_format_in="%Y-%m-%dT%H:%M:%S.000000000"
-dt_format_out="%Y%m%d-%H:%M:%S"
+dateformat_in="%Y-%m-%dT%H:%M:%S.000000000"
+dateformat_out="%Y%m%d-%H:%M:%S"
 
 for filename in filelist[rank::nranks]:
     with xr.open_dataset(filename) as ds_file:
@@ -62,11 +56,9 @@ for filename in filelist[rank::nranks]:
         print("rank %d executes cut of %s in %d parts" % (rank, filename, nparts), flush=True)
         for it in range(nparts):
             slice_name = str(ds_file.isel(time_counter=it).time_counter.values)
-            dt = datetime.strptime(slice_name,dt_format_in) # format check
-            print(f"rank {rank} processing slice: {slice_name}", flush=True)
+            dt = datetime.strptime(slice_name,dateformat_in)
 
-            dt_name = dt.strftime(dt_format_out)
-            outputfile = OUTPUTDIR + input_var + dt_name + ".nc"
+            outputfile = OUTPUTDIR / f"{input_var}{dt.strftime(dateformat_out)}.nc"
             print("rank %d generates %s" % (rank, outputfile), flush=True)
 
             ds_slice = ds_file.isel(time_counter=slice(it, it+1))
@@ -77,4 +69,3 @@ for filename in filelist[rank::nranks]:
             #     print(f"Variable: {var}, Encoding: {ds_slice[var].encoding['_FillValue'] if '_FillValue' in ds_slice[var].encoding else 'None'  }")
             ds_slice.to_netcdf(outputfile, mode="w", format="NETCDF4")
             ds_slice.close()
-            print("rank %d generates %s" % (rank, outputfile), flush=True)
